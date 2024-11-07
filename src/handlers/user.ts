78cabc411c27c7962812b77b20c01dfc61c78ff4
Tegar Aditya <tegaraditya53@gmail.com>
2024-11-07 18:39:34 +0700

    feat: simplify avatar assignment in user rank retrieval

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -206,7 +206,7 @@ export const getAllUserRankWithXP = factory.createHandlers(
 
       const result = userRank.map(({ id, avatar, name, scan_xp_total, rank }) => ({
         id: String(id),
-        avatar: avatar ? String(avatar) : null,
+        avatar: avatar,
         name: String(name),
         xp: Number(scan_xp_total),
         rank: scan_xp_total ? Number(rank) : null,
