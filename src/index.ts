34d655cd873a482b5d96ad598e0309fe07d221e3
Tegar Aditya <tegaraditya53@gmail.com>
2025-01-03 14:28:04 +0700

    feat: add basic authentication to /docs route and update .env.example with DOCS_PASSWORD

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { cors } from "hono/cors"
 import { secureHeaders } from "hono/secure-headers"
 import { trimTrailingSlash } from "hono/trailing-slash"
 import { serveStatic } from "hono/bun"
+import { basicAuth } from "hono/basic-auth"
 import api from "./routes/api"
 import docs from "./routes/docs"
 import healthcheck from "./routes/healthcheck"
@@ -13,6 +14,7 @@ const app = new Hono()
 app.use(logger()).use(cors()).use(secureHeaders()).use(trimTrailingSlash())
 
 app.use("/*", serveStatic({ root: "./static/" }))
+app.use("/docs/*", basicAuth({ username: "admin", password: String(process.env.DOCS_PASSWORD) }))
 
 app.route("/api", api)
 app.route("/docs", docs)
